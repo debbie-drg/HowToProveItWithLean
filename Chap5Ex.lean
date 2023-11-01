9312111935caaa61aa78d2a4674270d6d1030551
David Méndez <david.mendez@research.fchampalimaud.org>
2023-11-01 10:44:11 +0000

    Section 5.4 done

--- Chap5Ex.lean
+++ Chap5Ex.lean
@@ -295,73 +295,295 @@ theorem Exercise_5_2_21b {A B C : Type} (f : B → C) (a : A)
 /- Section 5.3 -/
 -- 1.
 theorem Theorem_5_3_2_2 {A B : Type} (f : A → B) (g : B → A)
-    (h1 : graph g = inv (graph f)) : f ∘ g = id := sorry
+    (h1 : graph g = inv (graph f)) : f ∘ g = id := by
+  apply funext; fix b
+  have h : (b, g b) ∈ graph g := rfl
+  rw [h1] at h
+  define at h
+  exact h
+  done
 
 -- 2.
 theorem Theorem_5_3_3_2 {A B : Type} (f : A → B) (g : B → A)
-    (h1 : f ∘ g = id) : onto f := sorry
+    (h1 : f ∘ g = id) : onto f := by
+  define; fix b; apply Exists.intro (g b)
+  rw [← comp_def f g, h1]
+  rfl
+  done
 
 -- 3.
 theorem Exercise_5_3_11a {A B : Type} (f : A → B) (g : B → A) :
-    one_to_one f → f ∘ g = id → graph g = inv (graph f) := sorry
+    one_to_one f → f ∘ g = id → graph g = inv (graph f) := by
+  assume f11 : one_to_one f
+  assume fgid : f ∘ g = id
+  have gfid : g ∘ f = id := by
+    apply funext; fix a
+    have h : f (g (f a)) = f a := by
+      show f (g (f a)) = f a from
+        calc f (g (f a))
+          _ = (f ∘ g) (f a) := by rfl
+          _ = id (f a) := by rw [fgid]
+          _ = f a := by rfl
+    define at f11
+    exact f11 (g (f a)) a h
+  exact Theorem_5_3_5 f g gfid fgid
+  done
 
 -- 4.
 theorem Exercise_5_3_11b {A B : Type} (f : A → B) (g : B → A) :
-    onto f → g ∘ f = id → graph g = inv (graph f) := sorry
+    onto f → g ∘ f = id → graph g = inv (graph f) := by
+  assume fonto : onto f
+  assume gfid : g ∘ f = id
+  apply Set.ext; fix (b, a)
+  define at fonto
+  apply Iff.intro;
+  · assume h : (b, a) ∈ graph g
+    define; define at h
+    obtain (a' : A) (ha' : f a' = b) from fonto b
+    have haa' : a = a' := by
+      show a = a' from
+        calc a
+         _ = g b := by rw [h]
+         _ = g (f a') := by rw [← ha']
+         _ = (g ∘ f) a' := by rfl
+         _ = id a' := by rw [gfid]
+         _ = a' := by rfl
+    rw [haa']; exact ha'
+  · assume h : (b, a) ∈ inv (graph f)
+    define; define at h
+    show g b = a from
+      calc g b
+        _ = g (f a) := by rw [h]
+        _ = (g ∘ f) a := by rfl
+        _ = id a := by rw [gfid]
+        _ = a := by rfl
+  done
 
 -- 5.
 theorem Exercise_5_3_14a {A B : Type} (f : A → B) (g : B → A)
-    (h : f ∘ g = id) : ∀ x ∈ Ran (graph g), g (f x) = x := sorry
+    (h : f ∘ g = id) : ∀ x ∈ Ran (graph g), g (f x) = x := by
+  fix a : A; assume ha : a ∈ Ran (graph g)
+  define at ha; obtain (b : B) (hb : (b, a) ∈ graph g) from ha
+  define at hb; rw [← hb]
+  show g (f (g b)) = g b from
+    calc g (f (g b))
+     _ = g ((f ∘ g) b) := by rfl
+     _ = g (id b) := by rw [h]
+     _ = g b := by rfl
+  done
 
 -- 6.
 theorem Exercise_5_3_18 {A B C : Type} (f : A → C) (g : B → C)
     (h1 : one_to_one g) (h2 : onto g) :
-    ∃ (h : A → B), g ∘ h = f := sorry
+    ∃ (h : A → B), g ∘ h = f := by
+  obtain (ginv : C → B) (hginv : graph ginv = inv (graph g))
+    from Theorem_5_3_1 g h1 h2
+  have hcompid : g ∘ ginv = id := Theorem_5_3_2_2 g ginv hginv
+  apply Exists.intro (ginv ∘ f)
+  show g ∘ ginv ∘ f = f from
+    calc g ∘ ginv ∘ f
+      _ = (g ∘ ginv) ∘ f := by rfl
+      _ = id ∘ f := by rw [hcompid]
+      _ = f := by rfl
+  done
 
 -- Definition for next two exercises:
 def conjugate (A : Type) (f1 f2 : A → A) : Prop :=
     ∃ (g g' : A → A), (f1 = g' ∘ f2 ∘ g) ∧ (g ∘ g' = id) ∧ (g' ∘ g = id)
 
 -- 7.
-theorem Exercise_5_3_17a {A : Type} : symmetric (conjugate A) := sorry
+theorem Exercise_5_3_17a {A : Type} : symmetric (conjugate A) := by
+  define; fix f₁; fix f₂
+  assume h : conjugate A f₁ f₂
+  define at h
+  obtain (g : A → A) (hg : ∃ (g' : A → A), f₁ = g' ∘ f₂ ∘ g ∧ g ∘ g' = id ∧ g' ∘ g = id) from h
+  obtain (g' : A → A) (hg' : f₁ = g' ∘ f₂ ∘ g ∧ g ∘ g' = id ∧ g' ∘ g = id) from hg
+  define
+  apply Exists.intro g'; apply Exists.intro g
+  apply And.intro
+  · have h₁ : f₁ = g' ∘ f₂ ∘ g := hg'.left
+    show f₂ = g ∘ f₁ ∘ g' from
+      calc f₂
+        _ = id ∘ f₂ ∘ id := by rfl
+        _ = (g ∘ g') ∘ f₂ ∘ (g ∘ g') := by rw [hg'.right.left]
+        _ = g ∘ (g' ∘ f₂ ∘ g) ∘ g' := by rfl
+        _ = g ∘ f₁ ∘ g' := by rw [h₁]
+  · apply And.intro hg'.right.right hg'.right.left
+  done
 
 -- 8.
 theorem Exercise_5_3_17b {A : Type} (f1 f2 : A → A)
     (h1 : conjugate A f1 f2) (h2 : ∃ (a : A), f1 a = a) :
-    ∃ (a : A), f2 a = a := sorry
+    ∃ (a : A), f2 a = a := by
+  define at h1
+  obtain (g : A → A) (hg : ∃ (g' : A → A), f1 = g' ∘ f2 ∘ g ∧ g ∘ g' = id ∧ g' ∘ g = id) from h1
+  obtain (g': A → A) (hg' : f1 = g' ∘ f2 ∘ g ∧ g ∘ g' = id ∧ g' ∘ g = id) from hg
+  obtain (a : A) (hf1a : f1 a = a) from h2
+  apply Exists.intro (g a)
+  have conjhip : f1 = g' ∘ f2 ∘ g := hg'.left
+  rw [conjhip] at hf1a
+  show f2 (g a) = g a from
+    calc f2 (g a)
+      _ = id (f2 (g a)) := by rfl
+      _ = (g ∘ g') (f2 (g a)) := by rw [hg'.right.left]
+      _ = g ((g' ∘ f2 ∘ g) a) := by rfl
+      _ = g a := by rw [hf1a]
+  done
 
 /- Section 5.4 -/
 -- 1.
 example {A : Type} (F : Set (Set A)) (B : Set A) :
-    smallestElt (sub A) B F → B = ⋂₀ F := sorry
+    smallestElt (sub A) B F → B = ⋂₀ F := by
+  assume h1 : smallestElt (sub A) B F
+  define at h1
+  ext a; apply Iff.intro
+  · assume h2 : a ∈ B
+    define; fix C : Set A; assume h3 : C ∈ F
+    have h4 : sub A B C := h1.right C h3
+    define at h4; exact h4 h2
+  · assume h2 : a ∈ ⋂₀ F
+    define at h2
+    exact h2 B h1.left
+  done
 
 -- 2.
 def complement {A : Type} (B : Set A) : Set A := { a : A | a ∉ B }
 
 theorem Exercise_5_4_7 {A : Type} (f g : A → A) (C : Set A)
-    (h1 : f ∘ g = id) (h2 : closed f C) : closed g (complement C) := sorry
+    (h1 : f ∘ g = id) (h2 : closed f C) : closed g (complement C) := by
+  define; fix x : A; assume h3 : x ∈ complement C
+  define at h3
+  define; by_contra h4
+  have h5 : x ∈ C := by
+    have h5 : f (g x) ∈ C := h2 (g x) h4
+    rw [← comp_def f g, h1] at h5
+    exact h5
+  show False from h3 h5
+  done
 
 -- 3.
 theorem Exercise_5_4_9a {A : Type} (f : A → A) (C1 C2 : Set A)
-    (h1 : closed f C1) (h2 : closed f C2) : closed f (C1 ∪ C2) := sorry
+    (h1 : closed f C1) (h2 : closed f C2) : closed f (C1 ∪ C2) := by
+  define; fix x : A; assume h3 : x ∈ C1 ∪ C2
+  by_cases on h3
+  · left; exact h1 x h3
+  · right; exact h2 x h3
+  done
 
 -- 4.
 theorem Exercise_5_4_10a {A : Type} (f : A → A) (B1 B2 C1 C2 : Set A)
     (h1 : closure f B1 C1) (h2 : closure f B2 C2) :
-    B1 ⊆ B2 → C1 ⊆ C2 := sorry
+    B1 ⊆ B2 → C1 ⊆ C2 := by
+  define at h1; define at h2
+  assume h3 : B1 ⊆ B2
+  have h4 : C2 ∈ {D : Set A | B1 ⊆ D ∧ closed f D} := by
+    have h5 : C2 ∈ {D : Set A | B2 ⊆ D ∧ closed f D} := h2.left
+    define at h5
+    define; apply And.intro
+    · exact Set.Subset.trans h3 h5.left
+    · exact h5.right
+  exact h1.right C2 h4
+  done
 
 -- 5.
 theorem Exercise_5_4_10b {A : Type} (f : A → A) (B1 B2 C1 C2 : Set A)
     (h1 : closure f B1 C1) (h2 : closure f B2 C2) :
-    closure f (B1 ∪ B2) (C1 ∪ C2) := sorry
+    closure f (B1 ∪ B2) (C1 ∪ C2) := by
+  define at h1; define at h2
+  have h3 : C1 ∈ {D : Set A | B1 ⊆ D ∧ closed f D} := h1.left
+  define at h3
+  have h4 : C2 ∈ {D : Set A | B2 ⊆ D ∧ closed f D} := h2.left
+  define at h4
+  define
+  apply And.intro
+  · define
+    apply And.intro
+    · exact Set.union_subset_union h3.left h4.left
+    · define; fix x;
+      assume h5 : x ∈ C1 ∪ C2
+      by_cases on h5
+      · left; exact h3.right x h5
+      · right; exact h4.right x h5
+  · fix D; assume h5 : D ∈ {D : Set A | B1 ∪ B2 ⊆ D ∧ closed f D}
+    define at h5
+    define; fix a; assume h6 : a ∈ C1 ∪ C2
+    by_cases on h6
+    · have h7 : B1 ⊆ D := by
+        have h8 : B1 ⊆ B1 ∪ B2 := Set.subset_union_left B1 B2
+        exact Set.Subset.trans h8 h5.left
+      have h8 : D ∈ {D : Set A | B1 ⊆ D ∧ closed f D} := by
+        define
+        exact And.intro h7 h5.right
+      have h9 : sub A C1 D := h1.right D h8
+      define at h9
+      exact h9 h6
+    · have h7 : B2 ⊆ D := by
+        have h8 : B2 ⊆ B1 ∪ B2 := Set.subset_union_right B1 B2
+        exact Set.Subset.trans h8 h5.left
+      have h8 : D ∈ {D : Set A | B2 ⊆ D ∧ closed f D} := by
+        define
+        exact And.intro h7 h5.right
+      have h9 : sub A C2 D := h2.right D h8
+      define at h9
+      exact h9 h6
+  done
 
 -- 6.
 theorem Theorem_5_4_9 {A : Type} (f : A → A → A) (B : Set A) :
-    ∃ (C : Set A), closure2 f B C := sorry
+    ∃ (C : Set A), closure2 f B C := by
+  set F : Set (Set A) := { D : Set A | B ⊆ D ∧ closed2 f D }
+  set C : Set A := ⋂₀ F
+  apply Exists.intro C
+  define
+  apply And.intro
+  · define
+    apply And.intro
+    · fix b; assume h1 : b ∈ B
+      define; fix D; assume h2 : D ∈ F
+      define at h2
+      exact h2.left h1
+    · define; fix a : A; assume h1 : a ∈ C; fix b : A; assume h2 : b ∈ C
+      define at h1; define at h2
+      define; fix D; assume h3 : D ∈ F
+      have h4 : a ∈ D := h1 D h3
+      have h5 : b ∈ D := h2 D h3
+      define at h3
+      exact h3.right a h4 b h5
+  · fix D : Set A
+    assume h1 : D ∈ {D : Set A | B ⊆ D ∧ closed2 f D}
+    define; fix a : A; assume h2 : a ∈ C
+    define at h2
+    have h3 : D ∈ F := h1
+    exact h2 D h3
+  done
 
 -- 7.
 theorem Exercise_5_4_13a {A : Type} (F : Set (A → A)) (B : Set A) :
-    ∃ (C : Set A), closure_family F B C := sorry
+    ∃ (C : Set A), closure_family F B C := by
+  set Fc : Set (Set A) := { D : Set A | B ⊆ D ∧ closed_family F D }
+  set C : Set A := ⋂₀ Fc
+  apply Exists.intro C
+  define
+  apply And.intro
+  · define
+    apply And.intro
+    · fix b; assume h1 : b ∈ B
+      define; fix D; assume h2 : D ∈ Fc
+      define at h2
+      exact h2.left h1
+    · define; fix f : A → A; assume h1 : f ∈ F; fix b : A; assume h2 : b ∈ C
+      define at h2
+      define; fix D; assume h3 : D ∈ Fc
+      have h4 : b ∈ D := h2 D h3
+      define at h3
+      exact h3.right f h1 b h4
+  · fix D : Set A
+    assume h1 : D ∈ {D : Set A | B ⊆ D ∧ closed_family F D}
+    define; fix a : A; assume h2 : a ∈ C
+    define at h2
+    have h3 : D ∈ Fc := h1
+    exact h2 D h3
+  done
 
 /- Section 5.5 -/
 
--- Explanations/Chap5Explanations.lean
+++ Explanations/Chap5Explanations.lean
@@ -198,3 +198,262 @@ theorem Theorem_5_2_5_2 {A B C : Type} (f : A → B) (g : B → C) :
   apply Exists.intro a
   rw [comp_def, h', h]
   done
+
+/-
+We continue with inverses. Since functions are relations, we can always consider
+the inverse relation. However, when can we ensure that such inverse is a
+function? Remember that `inv` can be used to obtain the inverse of a relation.
+
+We're gonna prove that if a function is onto and one-to-one, it's inverse is
+a function. For that, we are going to use the `func_from_graph` result proved
+in Section 5.1.
+-/
+
+#check @func_from_graph
+
+theorem Theorem_5_3_1 {A B : Type}
+    (f : A → B) (h1 : one_to_one f) (h2 : onto f) :
+    ∃ (g : B → A), graph g = inv (graph f) := by
+  rw [func_from_graph]
+  define; fix b : B
+  exists_unique
+  · define at h2
+    obtain (a : A) (h4 : f a = b) from h2 b
+    apply Exists.intro a
+    define; exact h4
+  · fix a₁; fix a₂
+    assume h3 : (b, a₁) ∈ inv (graph f)
+    assume h4 : (b, a₂) ∈ inv (graph f)
+    define at h3; define at h4
+    rewrite [← h4] at h3
+    define at h1
+    exact h1 a₁ a₂ h3
+  done
+
+/-
+Now, what is the relation between a function and its inverse? They should
+compose to the identity, which as mention can be written as `id`.
+-/
+
+theorem Theorem_5_3_2_1 {A B : Type} (f : A → B) (g : B → A)
+    (h1 : graph g = inv (graph f)) : g ∘ f = id := by
+  apply funext; fix a : A
+  have h2 : (f a, a) ∈ graph g := by
+    rw [h1]
+    define
+    rfl
+  define at h2
+  exact h2
+  done
+
+theorem Theorem_5_3_2_2 {A B : Type} (f : A → B) (g : B → A)
+    (h1 : graph g = inv (graph f)) : f ∘ g = id := by
+  have h2 : graph f = inv (graph g) := by
+    apply Set.ext; fix (a, b)
+    show (a, b) ∈ graph f ↔ (a, b) ∈ inv (graph g) from
+      calc (a, b) ∈ graph f
+        _ ↔ (b, a) ∈ inv (graph f) := by rfl
+        _ ↔ (a, b) ∈ inv (inv (graph f)) := by rfl
+        _ ↔ (a, b) ∈ inv (graph g) := by rw [h1]
+  apply funext; fix b : B
+  have h' : (g b, b) ∈ graph f := by
+    rw [h2]
+    define
+    rfl
+  define at h'
+  exact h'
+  done
+
+/-
+Using he results above, if `f` is one-to-one and onto, it has an inverse `g`.
+The converse is true as well.
+-/
+
+theorem Theorem_5_3_3_1 {A B : Type} (f : A → B) (g : B → A)
+    (h1 : g ∘ f = id) : one_to_one f := by
+  define; fix a₁; fix a₂
+  assume h2 : f a₁ = f a₂
+  show a₁ = a₂ from
+    calc a₁
+      _ = id a₁ := by rfl
+      _ = (g ∘ f) a₁ := by rw [h1]
+      _ = g (f a₁) := by rfl
+      _ = g (f a₂) := by rw [h2]
+      _ = (g ∘ f) a₂ := by rfl
+      _ = id a₂ := by rw [← h1]
+      _ = a₂ := by rfl
+  done
+
+theorem Theorem_5_3_3_2 {A B : Type} (f : A → B) (g : B → A)
+    (h1 : f ∘ g = id) : onto f := by
+  define; fix b : B
+  apply Exists.intro (g b)
+  show f (g b) = b from
+    calc f (g b)
+      _ = (f ∘ g) b := by rfl
+      _ = id b := by rw [h1]
+      _ = b := by rfl
+  done
+
+/-
+And combining, we have the following.
+-/
+
+theorem Theorem_5_3_5 {A B : Type} (f : A → B) (g : B → A)
+    (h1 : g ∘ f = id) (h2 : f ∘ g = id) : graph g = inv (graph f) := by
+  have h3 : one_to_one f := Theorem_5_3_3_1 f g h1
+  have h4 : onto f := Theorem_5_3_3_2 f g h2
+  obtain (g' : B → A) (h5 : graph g' = inv (graph f))
+    from Theorem_5_3_1 f h3 h4
+  have h6 : g' ∘ f = id := Theorem_5_3_2_1 f g' h5
+  have h7 : g = g' :=
+    calc g
+      _ = id ∘ g := by rfl
+      _ = (g' ∘ f) ∘ g := by rw [h6]
+      _ = g' ∘ (f ∘ g) := by rfl
+      _ = g' ∘ id := by rw [h2]
+      _ = g' := by rfl
+  rw [h7]
+  exact h5
+  done
+
+/-
+Assume now that `f : A → A` and `C : Set A`. We say that `C` is closed under
+`f` if the image by `f` of any element of `C` is again in `C`.
+-/
+
+def closed {A : Type} (f : A → A) (C : Set A) : Prop := ∀ x ∈ C, f x ∈ C
+
+/-
+If a set isn't closed, we can add elements until it is. The `closure` is the
+smallest set verifying this, with respect to the relation `sub` that we already
+worked with. Thus:
+-/
+
+def closure {A : Type} (f : A → A) (B C : Set A) : Prop :=
+  smallestElt (sub A) C { D : Set A | B ⊆ D ∧ closed f D }
+
+/-
+We have already shown that if a smallest element exists, it is unique.
+But does the closure of a set exists? We now prove it. The way to go is to
+obtain it as the intersection of every clossed set verifying the proerty.
+-/
+
+theorem Theorem_5_4_5 {A : Type} (f : A → A) (B : Set A) :
+    ∃ (C : Set A), closure f B C := by
+  set F : Set (Set A) := { D : Set A | B ⊆ D ∧ closed f D }
+  set C : Set A := ⋂₀ F
+  apply Exists.intro C
+  define
+  apply And.intro
+  · define
+    apply And.intro
+    · fix a : A; assume h1 : a ∈ B
+      define; fix D : Set A; assume h2 : D ∈ F
+      define at h2
+      exact h2.left h1
+    · define; fix a : A; assume h1 : a ∈ C
+      define; fix D : Set A; assume h2 : D ∈ F
+      define at h1
+      have h3 : a ∈ D := h1 D h2
+      define at h2
+      exact h2.right a h3
+  · fix D : Set A
+    assume h1 : D ∈ {D : Set A | B ⊆ D ∧ closed f D}
+    define; fix a : A; assume h2 : a ∈ C
+    define at h2
+    exact h2 D h1
+  done
+
+/-
+Usually, closure is most desirable when having functions of two variables,
+of type `(A × A) → A`, or equivalently, `A → A → A`. These are commonly
+used when talking about algebraic substructures, as one of the requirements
+is precisely that they're closed under the algebraic structure's different
+operations. One such example is that of integer addition.
+-/
+
+def plus (m n : Int) : Int := m + n
+
+def plus' : Int → Int → Int := fun (m n : Int) => m + n
+
+def plus'' : Int → Int → Int := fun (m : Int) => (fun (n : Int) => m + n)
+
+/-
+The third way is perhaps the closest to the type. Namely, for every integer
+we have a function from integers to integers.
+-/
+
+/-
+If we have `f : A → A → A` and `a : A`, `f a : A → A`. We call this
+partial application of `f`. Of course, if `b : A`, `f a b : A`, where this
+can be thought of applying `f a` to `b`.
+-/
+
+/-
+Now, remember that in Chapter 3 we introduced predicates, `Pred U`.
+Internally, `Pred U` is defined to be the type `U → Prop`, so if `P : Pred U`
+and `x : U`, `P x : Prop` is the proposition corresponding to applying `P`
+to `u`.
+
+Similarly, `Rel` stands for `A → B → Prop`, so if `R : Rel A B`, `R` is a
+function of two variables: one of type `A` and one of type `B`.
+
+Another already seen example is that of `closed f C`. Here, the second
+argument is a function `f : A → A`. Functions can be used as arguments
+for functions in Lean, as is common in functional programming paradigms.
+-/
+
+/-
+Back to closures, this is how we can extend the idea of closures to
+functions of two variables.
+-/
+
+def closed2 {A : Type} (f : A → A → A) (C : Set A) : Prop :=
+  ∀ x ∈ C, ∀ y ∈ C, f x y ∈ C
+
+def closure2 {A : Type} (f : A → A → A) (B C : Set A) : Prop :=
+  smallestElt (sub A) C { D : Set A | B ⊆ D ∧ closed2 f D }
+
+/-
+Closures under functions of two variables also exist. The argument is
+pretty similar.
+-/
+
+theorem Theorem_5_4_9 {A : Type} (f : A → A → A) (B : Set A) :
+    ∃ (C : Set A), closure2 f B C := by
+  set F : Set (Set A) := { D : Set A | B ⊆ D ∧ closed2 f D }
+  set C : Set A := ⋂₀ F
+  apply Exists.intro C
+  define
+  apply And.intro
+  · define
+    apply And.intro
+    · fix b; assume h1 : b ∈ B
+      define; fix D; assume h2 : D ∈ F
+      define at h2
+      exact h2.left h1
+    · define; fix a : A; assume h1 : a ∈ C; fix b : A; assume h2 : b ∈ C
+      define at h1; define at h2
+      define; fix D; assume h3 : D ∈ F
+      have h4 : a ∈ D := h1 D h3
+      have h5 : b ∈ D := h2 D h3
+      define at h3
+      exact h3.right a h4 b h5
+  · fix D : Set A
+    assume h1 : D ∈ {D : Set A | B ⊆ D ∧ closed2 f D}
+    define; fix a : A; assume h2 : a ∈ C
+    define at h2
+    have h3 : D ∈ F := h1
+    exact h2 D h3
+  done
+
+/-
+We also considered closed families and the closure of a family.
+-/
+
+def closed_family {A : Type} (F : Set (A → A)) (C : Set A) : Prop :=
+  ∀ f ∈ F, closed f C
+
+def closure_family {A : Type} (F : Set (A → A)) (B C : Set A) : Prop :=
+  smallestElt (sub A) C { D : Set A | B ⊆ D ∧ closed_family F D }
